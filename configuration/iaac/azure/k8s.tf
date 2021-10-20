14e526459aae0520314da20654f05a00e7285e35
Kannadasa <kannadasan.swaminathan@gmail.com>
2021-10-20 06:26:58 +0100

    deployment

--- configuration/iaac/azure/k8s.tf
+++ configuration/iaac/azure/k8s.tf
@@ -3,8 +3,7 @@ resource "azurerm_resource_group_template_deployment" "deploy" {
   name = "incrdeploy"
   resource_group_name = "k8s-rg2"
   deployment_mode = "Incremental"
-  template_spec_version_id = azurerm_kubernetes_cluster.k8s
-  
+  template_spec_version_id = "${azurerm_resource_group.k8s.name}"
 }
 resource "azurerm_resource_group" "k8s" {
   name     = "${var.resource_group_name}"
